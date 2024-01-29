3cfd68ab0de549a6e778b08d70dd323a8e5e5fd0
Tomi <amandipthandi@gmail.com>
2024-01-29 11:21:18 +0700

    Add user input validation for height in mario-more.c

--- week-1/mario/mario-more.c
+++ week-1/mario/mario-more.c
@@ -1,13 +1,15 @@
-//    #  #
-//   ##  ##
-//  ###  ###
-// ####  ####
-
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-  int height = 4;
+  int height;
+
+  do
+  {
+    height = get_int("Height: ");
+  }
+  while (height < 1 || height > 8);
 
   for (int row = 1; row <= height; row++)
   {
@@ -20,7 +22,7 @@ int main(void)
       printf("#");
     }
     printf("  ");
-    
+
     for (int secHash = 1; secHash <= row; secHash++)
     {
       printf("#");
